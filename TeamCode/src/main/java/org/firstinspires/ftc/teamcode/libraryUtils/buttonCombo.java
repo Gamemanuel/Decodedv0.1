130c4cd025997738e11a338597245216f02b1d5f
sp136r <imad0ofus09@gmail.com>
2025-11-24 18:31:13 -0700

    button combo class for toggling things with 2 inputs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libraryUtils/buttonCombo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libraryUtils/buttonCombo.java
@@ -1,4 +1,25 @@
 package org.firstinspires.ftc.teamcode.libraryUtils;
 
 public class buttonCombo {
+    private final boolean button1;
+    private final boolean button2;
+    private boolean attempt = false;
+    private final boolean toggle;
+
+    public buttonCombo(boolean button1, boolean button2, boolean toggle) {
+        this.button1 = button1;
+        this.button2 = button2;
+        this.toggle = toggle;
+
+        if (button1 && button2 && !attempt) {
+            toggle = !toggle;
+            attempt = true;
+        } else if (!(button1 && button2)) {
+            attempt = false;
+        }
+    }
+    public boolean get() {
+        return toggle;
+    }
+
 }
