fc4302c83c3d005eb43295797c58171542d7c28b
sp136r <imad0ofus09@gmail.com>
2025-11-25 13:43:28 -0700

    literally just toggles an input

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libraryUtils/Toggle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libraryUtils/Toggle.java
@@ -1,4 +1,15 @@
 package org.firstinspires.ftc.teamcode.libraryUtils;
 
 public class Toggle {
+    private boolean bool;
+
+    public Toggle(boolean bool) {
+        this.bool = bool;
+    }
+    public void toggle() {
+        bool = !bool;
+    }
+    public boolean get() {
+        return bool;
+    }
 }
