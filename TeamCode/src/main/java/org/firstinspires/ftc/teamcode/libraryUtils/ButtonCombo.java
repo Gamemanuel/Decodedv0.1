e9452852ce30f255d484b7029aa93fbe62d5960d
sp136r <imad0ofus09@gmail.com>
2025-11-25 11:31:50 -0700

    added documentation for ButtonCombo class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libraryUtils/ButtonCombo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libraryUtils/ButtonCombo.java
@@ -1,14 +1,18 @@
 package org.firstinspires.ftc.teamcode.libraryUtils;
 
-/**
- * Handles toggling using 2 button inputs.
- */
 public class ButtonCombo {
     private final boolean button1;
     private final boolean button2;
     private boolean attempt = false;
     private boolean toggle = false;
 
+    /**
+     * Creates a toggle using a button combination
+     * @author Will
+     * @param button1 First button in combination
+     * @param button2 Second button in combination
+     * @version 0.1
+     */
     public ButtonCombo(boolean button1, boolean button2) {
         this.button1 = button1;
         this.button2 = button2;
