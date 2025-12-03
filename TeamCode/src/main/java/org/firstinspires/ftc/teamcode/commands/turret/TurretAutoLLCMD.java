6431bb47aa239b505f44204795a31b2c78da80b3
sp136r <imad0ofus09@gmail.com>
2025-12-03 11:25:51 -0700

    faceGoal() now tries to find the april tag again after losing it and simplified some code in 'TurretAutoLLCMD.java'

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/turret/TurretAutoLLCMD.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/turret/TurretAutoLLCMD.java
@@ -12,13 +12,13 @@ public class TurretAutoLLCMD {
 
     TurretSubsystem turret;
     LLSubsystem ll;
-
-    double offset = 0;
-
+    double prevSpeed = 0;
 
     public TurretAutoLLCMD(TurretSubsystem turret, LLSubsystem ll) {
         this.turret = turret;
         this.ll = ll;
+
+
     }
 
     public double[] faceGoal(double tolerance, Alliance alliance) {
@@ -32,17 +32,15 @@ public class TurretAutoLLCMD {
         LLResult result = ll.limelight.getLatestResult();
         if (result != null && result.isValid()) {
             tx = result.getTx();
-            speed = Math.min(1, Math.max(Math.abs(tx) / 10, 0.25)); // slows down, min is 0.25 power, max is 1, slows down at 10 units
+            speed = Math.min(1, Math.max(tx / 10, 0.25)); // slows down, min is 0.25 power, max is 1, slows down at 10 units
             if (Math.abs(tx) > tolerance) {
-                if (tx > 0) {
-                    // negative is left
-                    turret.setPower(-speed);
-                } else {
-                    turret.setPower(speed);
-                }
+                turret.setPower(speed);
+                prevSpeed = speed;
             } else {
                 turret.setPower(0);
             }
+        } else {
+            turret.setPower(-prevSpeed);
         }
         return new double[] {tx, speed}; // telementry
     }
